d3b5b9fe18ebaac0cc6b13553f488d4dacbee34d
Hermes Jimenez Luna <hermes@HermesMac.local>
2017-09-28 21:55:43 -0500

    optimizing_pathfind_algorithm partial

--- kata_codewars/optimizing_pathfinding_algorithm.py
+++ kata_codewars/optimizing_pathfinding_algorithm.py
@@ -1,13 +1,26 @@
 def get_number_of_reachable_fields(grid, rows, columns, start_row, start_column):
     # Your magic here...
-    print(grid[start_row][start_column])
-    if grid[start_row][start_column] == 0:
-        return 0
+    for g in grid:
+        print(g) 
+    
     #create grid
-    gameboard = [rows][columns]
+    gameboard = [[0 for x in range(rows)] for y in range(columns)]
     # Draw board - Blocked
-    for i in range(0,rows)
+    for r in range(rows):
+        for c in range(columns):
+            gameboard[r][c] = '-'
+            if r == start_row and c == start_column:
+                gameboard[r][c] = "PL"
+            elif grid[r][c] == 0:
+                gameboard[r][c] = "B"
 
-
+    for g in gameboard:
+        print(g) 
+    return gameboard
     #try to move left or right
+
+grid = [[1,1,1],
+        [0,0,1],
+        [0,1,0]]
+get_number_of_reachable_fields(grid, 3, 3, 0, 0)
 
